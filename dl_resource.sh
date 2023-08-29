d24111749ea9f107c55f2cd5905b843d63e84b79
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-08-29 11:16:02 +0900

    refactor: 作業フォルダが思ったより良く出てくるので、定数化。

--- dl_resource.sh
+++ dl_resource.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+WORK_DIR=./results
+
 har_file=${1}
 
 grep "\"url" ${har_file} | grep "m_hai.php?file=0" | sed 's/^.*url": "//g' | sed 's/",$//g' > ./urls.txt
 
-rm -rf ./result
-mkdir ./result
-mv ./urls.txt ./result
-cd ./result
+rm -rf ${WORK_DIR}
+mkdir ${WORK_DIR}
+mv ./urls.txt ${WORK_DIR}
+cd ${WORK_DIR}
 
 for i in $(cat ./urls.txt) ; do
   file_name=$(echo ${i} | sed 's/^.*\?file\=//g' | sed 's/\&.*$//g')
