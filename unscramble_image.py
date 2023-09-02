264991e07a43105680d15836f04ba15c089ae641
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-09-02 23:36:08 +0900

    implement: ロジックを見直し。配列に順番にタイルを収めていくようなロジックに変更.

--- unscramble_image.py
+++ unscramble_image.py
@@ -2,28 +2,51 @@
 # -*- coding: utf-8 -*-:q
 
 from PIL import Image, ImageFilter
+import logging
 
 TILE_SIZE = 4
 
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
+
 target_file = "./results/0000_0000.bin"
+scramble = [12, 3, 1, 15, 6, 8, 10, 4, 9, 5, 14, 13, 0, 2, 7, 11]
 
 base_image = Image.open(target_file)
 unscramble_image = Image.open(target_file)
 
-tile_v_len = 176
-tile_h_len = base_image.height / TILE_SIZE
+tile_v_len = base_image.height // TILE_SIZE
+tile_h_len = 176
 
-image_tiles = [[],[],[],[]]
+image_tiles = []
 
-for i in range(TILE_SIZE):
-    for j in range(TILE_SIZE):
-        left = tile_v_len * i
-        top = tile_h_len * j
-        right = tile_v_len * (i + 1)
-        bottom = tile_h_len * (j + 1)
+for y in range(TILE_SIZE):
+    for i in range(TILE_SIZE):
+        left = tile_h_len * i
+        top = tile_v_len * y
+        right = tile_h_len * (i + 1)
+        bottom = tile_v_len * (y + 1)
 
         image_tile = base_image.crop((left, top, right, bottom))
-        image_tiles[i].append(image_tile)
+        image_tiles.append(image_tile)
 
-        file_name = str(i) + "_" + str(j) + ".bmp"
-        image_tiles[i][j].save(file_name)
+for i, image_tile in enumerate(image_tiles):
+    tile_file_name = str(i) + ".bmp"
+    image_tile.save(tile_file_name)
+
+for i, scramble_pos in enumerate(scramble):
+    i = i % TILE_SIZE
+    y = i // TILE_SIZE
+
+    # logging.debug('x = {}'.format(x))
+    # logging.debug('y = {}'.format(y))
+
+    left = tile_v_len * i
+    top = tile_h_len * y
+
+    # logging.debug('left = {}'.format(left))
+    # logging.debug('top = {}'.format(top))
+
+    # image_tile = image_tiles[scramble_pos]
+    # unscramble_image.paste(image_tile, (left, top))
+
+unscramble_image.save("result.bmp")
