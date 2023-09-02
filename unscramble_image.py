94c73b84141bdf70007bf3a1583255f57052c312
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-09-03 05:40:35 +0900

    implement: スクランブル数値をXMLから読むやつを実装。g

--- unscramble_image.py
+++ unscramble_image.py
@@ -2,9 +2,12 @@
 # -*- coding: utf-8 -*-:q
 
 import sys
+import os
+import re
 import logging
-from argparse import ArgumentParser
+import xml.etree.ElementTree as ET
 from PIL import Image
+from argparse import ArgumentParser
 
 TILE_SIZE = 4
 TILE_HORIZONTAL_LENGTH = 176
@@ -19,10 +22,12 @@ parser = ArgumentParser(
 def main(argv=sys.argv):
     args = parser.parse_args()
 
-    target_file = "./results/0000_0000.bin"
-    scrambles = [12, 3, 1, 15, 6, 8, 10, 4, 9, 5, 14, 13, 0, 2, 7, 11]
+    target_file_path = "./results/0004_0000.bin"
 
-    target_image = Image.open(target_file)
+    target_image = Image.open(target_file_path)
+
+    scrambles = load_scramble_numbers(target_file_path)
+    logging.debug("scrambles = {}".format(scrambles))
 
     tile_v_len = target_image.height // TILE_SIZE
     tile_h_len = TILE_HORIZONTAL_LENGTH
@@ -61,5 +66,16 @@ def unscramble_image(target_image, image_tiles, scrambles, tile_h_len, tile_v_le
         hit_image_tile = image_tiles[scramble_pos]
         target_image.paste(hit_image_tile, (left, top))
 
+
+def load_scramble_numbers(target_image_file_path):
+    image_file_name = os.path.basename(target_image_file_path)
+    logging.debug("image_file_name = {}".format(image_file_name))
+    xml_file_name = re.sub("_.*.bin", ".xml", image_file_name)
+    xml_file_path = os.path.join(os.path.dirname(target_image_file_path), xml_file_name)
+    logging.debug("xml_file_path = {}".format(xml_file_path))
+    xml_tree = ET.parse(xml_file_path)
+    scramble_csv = xml_tree.getroot().find("./Scramble").text
+    logging.debug("scramble_csv = {}".format(scramble_csv))
+    return [int(n) for n in scramble_csv.split(',')]
 
 main()
