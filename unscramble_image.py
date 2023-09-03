2a70e9a5b73ad092e3bc4a15b48f453db497095f
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-09-03 23:40:08 +0900

    refactor: メソッド分割。

--- unscramble_image.py
+++ unscramble_image.py
@@ -7,12 +7,14 @@ from argparse import ArgumentParser
 from PIL import Image
 
 TILE_SIZE = 4
+TILE_HORIZONTAL_LENGTH = 176
 
-logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
+logging.basicConfig(level=logging.DEBUG, format="%(levelname)s: %(message)s")
 
 parser = ArgumentParser(
-    prog="unscramble_image",
-    description="とある軽度な暗号化がされている画像を定義ファイルを元に復元する。")
+    prog="unscramble_image", description="とある軽度な暗号化がされている画像を定義ファイルを元に復元する。"
+)
+
 
 def main(argv=sys.argv):
     args = parser.parse_args()
@@ -20,12 +22,19 @@ def main(argv=sys.argv):
     target_file = "./results/0000_0000.bin"
     scrambles = [12, 3, 1, 15, 6, 8, 10, 4, 9, 5, 14, 13, 0, 2, 7, 11]
 
-    base_image = Image.open(target_file)
-    unscramble_image = Image.open(target_file)
+    target_image = Image.open(target_file)
 
-    tile_v_len = base_image.height // TILE_SIZE
-    tile_h_len = 176
+    tile_v_len = target_image.height // TILE_SIZE
+    tile_h_len = TILE_HORIZONTAL_LENGTH
 
+    image_tiles = capture_image_tiles(target_image, tile_h_len, tile_v_len)
+
+    unscramble_image(target_image, image_tiles, scrambles, tile_h_len, tile_v_len)
+
+    target_image.save("result.png")
+
+
+def capture_image_tiles(base_image, tile_h_len, tile_v_len):
     image_tiles = []
 
     for y in range(TILE_SIZE):
@@ -38,6 +47,10 @@ def main(argv=sys.argv):
             image_tile = base_image.crop((left, top, right, bottom))
             image_tiles.append(image_tile)
 
+    return image_tiles
+
+
+def unscramble_image(target_image, image_tiles, scrambles, tile_h_len, tile_v_len):
     for i, scramble_pos in enumerate(scrambles):
         x = i % TILE_SIZE
         y = i // TILE_SIZE
@@ -46,8 +59,7 @@ def main(argv=sys.argv):
         top = tile_v_len * y
 
         hit_image_tile = image_tiles[scramble_pos]
-        unscramble_image.paste(hit_image_tile, (left, top))
+        target_image.paste(hit_image_tile, (left, top))
 
-    unscramble_image.save("result.png")
 
 main()
