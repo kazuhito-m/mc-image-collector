dc6ee01bb9315633be6661b48feec688f438f09e
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-09-10 20:09:43 +0900

    implement: 処理とコマンドライン捌きを分離する。

---
+++ ct/src/collect-command.ts
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import { Settings } from "./config/settings";
+
+const defaultSettings = require('../conf/settings.json');
+
+export class CollectCommand {
+    public execute(settingsFilePath?: string): number {
+        const settings = this.loadSettingsJsonOf(settingsFilePath);
+        if (!settings) {
+            console.log('指定された設定ファイルをよめませんでした。path:' + settingsFilePath);
+            return 1;
+        }
+
+        return 0;
+    }
+
+    private loadSettingsJsonOf(settingsFilePath?: string): Settings | undefined {
+        if (!settingsFilePath) return defaultSettings;
+        if (!fs.existsSync(settingsFilePath)) return undefined;
+
+        const text = fs.readFileSync(settingsFilePath, 'utf-8');
+        const json = JSON.parse(text);
+        return json;
+    }
+}
--- ct/src/index.ts
+++ ct/src/index.ts
@@ -1,5 +1,6 @@
 import { Command } from 'commander';
 import * as figlet from 'figlet';
+import { CollectCommand } from './collect-command';
 
 console.log(figlet.textSync("CT Image Collector"));
 
@@ -8,7 +9,20 @@ const packageJson = require('../package.json');
 const program = new Command()
     .version(packageJson.version)
     .description("とあるサイトの画像イメージをクローリング＆ダウンロードするコマンドラインツール。")
-    .option("-c, --config  <jsonfilepath>", "設定ファイルのJSONファイル")
+    .option("-s, --settings  <jsonfilepath>", "設定ファイルのJSONファイル")
     .parse(process.argv);
 
 const options = program.opts();
+
+let exitCode = 0;
+if (!options.h && !options.v) {
+    try {
+        const main = new CollectCommand();
+        exitCode = main.execute(options.settings);
+    } catch (error) {
+        console.error("予期せぬエラーが発生しました。", error);
+        exitCode = 127;
+    }
+};
+
+process.exit(exitCode);
