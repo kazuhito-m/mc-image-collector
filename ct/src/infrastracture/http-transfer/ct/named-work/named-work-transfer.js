b9aad1e484330df0103e7247f9ed66000dd84328
kazuhito_m <kazuhito.sumpic@gmail.com>
2023-09-11 03:52:07 +0900

    bugfix: 必要ないファイルを削除。

--- ct/src/infrastracture/http-transfer/ct/named-work/named-work-transfer.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NamedWorkTransfer = void 0;
-var NamedWorkTransfer = /** @class */ (function () {
-    function NamedWorkTransfer() {
-    }
-    return NamedWorkTransfer;
-}());
-exports.NamedWorkTransfer = NamedWorkTransfer;
