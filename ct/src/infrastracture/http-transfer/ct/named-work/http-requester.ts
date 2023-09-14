6ebed71f3149eaede9dfae449faad01652d4c074
kazuhito-m <kazuhito.sumpic@gmail.com>
2023-09-14 21:32:43 +0900

    implement: https:get ではなく、Fetchで取得するように。
    が、動かない。修正要。

--- ct/src/infrastracture/http-transfer/ct/named-work/http-requester.ts
+++ ct/src/infrastracture/http-transfer/ct/named-work/http-requester.ts
@@ -4,15 +4,8 @@ import https from 'https';
 @injectable()
 export class HttpRequester {
     public async httpsGetTextOf(url: string): Promise<string> {
-        const charBuffer: string[] = [];
-        await https.get(url, (response) => {
-                response.on('data', (chunk) => {
-                    charBuffer.push(chunk);
-                });
-                response.on('end', () => {
-                    console.log('File downloaded and stored.');
-                });
-            }).on("error", (err) => { console.error(new Error(err.message)) });
-        return charBuffer.join('');
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.text();
+        return data;
     }
 }
